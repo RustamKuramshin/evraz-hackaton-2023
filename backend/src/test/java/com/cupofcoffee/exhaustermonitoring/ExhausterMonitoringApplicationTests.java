d65b00e3782520fe93079fb7e12c52353f03597f
Rustam Kuramshin <leo.in.empty@gmail.com>
2023-02-14 23:03:32 +0300

    init

---
+++ backend/src/test/java/com/cupofcoffee/exhaustermonitoring/ExhausterMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cupofcoffee.exhaustermonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExhausterMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

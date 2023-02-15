656c57b38f862dca8748176d756a71f4a7cef9c7
Rustam Kuramshin <leo.in.empty@gmail.com>
2023-02-15 23:14:06 +0300

    commit

---
+++ backend/src/main/java/com/cupofcoffee/exhaustermonitoring/ExhausterDaoImpl.java
@@ -0,0 +1,35 @@
+package com.cupofcoffee.exhaustermonitoring;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class ExhausterDaoImpl implements ExhausterDao {
+
+  @Override
+  public List<Object> getAllExhausterMetricsForAllMachines(Consumer<Object> c) {
+    return null;
+  }
+
+  @Override
+  public Object getExhausterMetricsByExhausterId(Consumer<Object> c, String exhausterId) {
+    return null;
+  }
+
+  @Override
+  public List<Object> getExhausterInfoForAllMachines() {
+    return null;
+  }
+
+  @Override
+  public Object getExhausterInfoByExhausterId(String exhausterId) {
+    return null;
+  }
+}

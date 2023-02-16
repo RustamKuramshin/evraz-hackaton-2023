d8881ec7d84750aa275c67528df63aa44fb42d2e
Rustam Kuramshin <leo.in.empty@gmail.com>
2023-02-16 23:23:17 +0300

    commit

--- refresh.sh
+++ refresh.sh
@@ -6,10 +6,12 @@ git checkout main
 
 git pull
 
-docker compose rm -f -s exhauster-monitoring-api
+docker compose rm -f -s exhauster-monitoring-api exhauster-monitoring-ui
+
+docker rmi -f evraz-hackaton-2023-exhauster-monitoring-ui:latest
 
 cd backend
 
 ./gradlew bootBuildImage
 
-docker compose up -d metrics-db exhauster-monitoring-api
+docker compose up -d metrics-db exhauster-monitoring-api exhauster-monitoring-ui

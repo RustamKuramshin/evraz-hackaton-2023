f0fad70ec58f57ea20be7c80bd33376d6631c737
DenisKut <tarelko_den@list.ru>
2023-02-16 22:38:43 +0300

    WebSock refactor

--- stubs-server/app.js
+++ stubs-server/app.js
@@ -1,68 +1,16 @@
 /* eslint-disable */
-//const ws = require('ws');
-const express = require('express');
-const { connection } = require('mongoose');
-const app = express();
-const WSServer = require('express-ws')(app)
-const aWss = WSServer.getWss();
-const router = require('express').Router();
+const WebSocket = require('ws');
 
-const sendingData = { message: 'Data for GET request' };
+const wss = new WebSocket.Server({ port: 8080 });
 
-app.ws('/', (ws, req) => {
-  console.log('SUCCESSFULY CONNECTED');
-  ws.send('Ты подключён');
-  ws.on('message', (msg) => {
-    msg = JSON.parse(msg);
-    switch (msg.method) {
-      case 'connection':
-        connectionHandler(ws, msg);
-        break;
-    }
+wss.on('connection', ws => {
+  console.log('Client connected');
+
+  ws.on('message', data => {
+    console.log(`client send: ${data}`);
   })
-})
 
-app.listen(8080, () => console.log('Server started on PORT 8080'));
-
-const getInfo = (req, res) => {
-  const msg = { text: 'aboba' };
-  res.status(200).send(msg);
-};
-
-router.get('/api/v1/info', getInfo);
-app.use(router);
-const connectionHandler = (ws, msg) => {
-  ws.id = msg.id; //id для каждой сессии
-  broadcastConnection(ws, msg);
-}
-
-const broadcastConnection = (ws, msg) => {
-  aWss.clients.forEach(client => {
-    if(client.id === msg.id) {
-      client.send(`Пользователь ${msg.username}`);
-    }
-  });
-}
-// const wss = new ws.Server({
-//   port: 8080,
-// }, () => console.log('Server started on PORT 8080'));
-
-// wss.on('connection', function connection(ws) {
-//   ws.on('message', function(msg) {
-//     msg = JSON.parse(msg);
-//     switch (msg.event) {
-//       case 'message':
-//         broadcastMessage(msg);
-//         break;
-//       case 'connection':
-//         broadcastMessage(msg);
-//         break;
-//     }
-//   })
-// });
-
-// function broadcastMessage(message) {
-//   wss.clients.forEach(client => {
-//     client.send(JSON.stringify(message));
-//   })
-// }
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  })
+});

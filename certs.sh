da580d5e79adcaf417f2ca3977f982a78c35f0c3
Alexander Yanchiy <alex.yanchiy.92@gmail.com>
2023-02-17 02:35:02 +0300

    Получение данных из топика

--- certs.sh
+++ certs.sh
@@ -2,5 +2,12 @@
 
 #awk 'NF {sub(/\r/, ""); printf "%s\\n",$0;}'  CA.pem
 
-openssl pkcs12 -export -in CA.pem -out cert.p12
-keytool -importkeystore -srckeystore cert.p12 -srcstoretype pkcs12 -destkeystore cert.jks
+openssl x509 -outform der -in CA.pem -out cert.der
+
+keytool -genkey -keyalg RSA -alias endeca -keystore kafka-client-truststore.jks
+keytool -delete -alias endeca -keystore kafka-client-truststore.jks
+keytool -import -v -trustcacerts -alias endeca-ca -file CA.pem -keystore kafka-client-truststore.jks
+
+keytool -genkey -keyalg RSA -alias endeca -keystore kafka-client-keystore.jks
+keytool -delete -alias endeca -keystore kafka-client-keystore.jks
+keytool -v -importkeystore -srckeystore cert.der -srcstoretype x509 -deststoretype JKS -destkeystore kafka-client-keystore.jks

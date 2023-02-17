3c4178f9ab5fe2dabaed4f035e14d23dfaa62071
Rustam Kuramshin <leo.in.empty@gmail.com>
2023-02-17 23:50:12 +0300

    commit

--- backend/src/main/java/com/cupofcoffee/exhaustermonitoring/ExhausterDaoImpl.java
+++ backend/src/main/java/com/cupofcoffee/exhaustermonitoring/ExhausterDaoImpl.java
@@ -3,15 +3,16 @@ package com.cupofcoffee.exhaustermonitoring;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
-public class ExhausterDaoImpl {
+public class ExhausterDaoImpl implements ExhausterDao {
 
 //  @Override
   public List<Object> getAllExhausterMetricsForAllMachines(Consumer<Object> c) {
@@ -31,5 +32,30 @@ public class ExhausterDaoImpl {
 //  @Override
   public Object getExhausterInfoByExhausterId(String exhausterId) {
     return null;
+  }
+
+  @Override
+  public List<Map<String, String>> getMetricsJsonBetweenStartDateAndEndDate(LocalDateTime start, LocalDateTime end) {
+    return null;
+  }
+
+  @Override
+  public List<String> getAllMetricsByMetricNameBetweenStartDateAndEndDate(String metricName, LocalDateTime start, LocalDateTime end) {
+    return null;
+  }
+
+  @Override
+  public Map<String, String> getLastMetricsJson() {
+    return null;
+  }
+
+  @Override
+  public String getLastMetricByMetricName(String metricName) {
+    return null;
+  }
+
+  @Override
+  public void readChangeStream(Consumer<Object> consumer) {
+
   }
 }

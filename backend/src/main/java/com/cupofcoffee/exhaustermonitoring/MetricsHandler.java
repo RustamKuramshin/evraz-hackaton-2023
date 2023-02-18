22fa49568b210ec80cc184858691113c8bb7d552
Vladislav Kalinin <v.kalinin@devs-vault.com>
2023-02-18 12:08:22 +0400

    Парсинг работает

--- backend/src/main/java/com/cupofcoffee/exhaustermonitoring/MetricsHandler.java
+++ backend/src/main/java/com/cupofcoffee/exhaustermonitoring/MetricsHandler.java
@@ -1,6 +1,11 @@
 package com.cupofcoffee.exhaustermonitoring;
 
 import com.mongodb.client.model.changestream.ChangeStreamDocument;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.RFC4180Parser;
+import com.opencsv.RFC4180ParserBuilder;
+import com.opencsv.exceptions.CsvException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.Document;
@@ -13,11 +18,19 @@ import org.springframework.data.mongodb.core.messaging.MessageListenerContainer;
 import org.springframework.data.mongodb.core.messaging.Subscription;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -31,6 +44,30 @@ public class MetricsHandler extends TextWebSocketHandler {
     private String db;
 
     private final MessageListenerContainer container;
+
+    private Map<String, SignalDto> signals;
+
+    @PostConstruct
+    public void init() throws FileNotFoundException {
+
+        File fileWithSignals = ResourceUtils.getFile("classpath:signals_kafka.csv");
+        RFC4180Parser rfc4180Parser = new RFC4180ParserBuilder().build();
+        try (CSVReader reader = new CSVReaderBuilder(new FileReader(fileWithSignals))
+            .withSkipLines(1)
+            .withCSVParser(rfc4180Parser)
+            .build()) {
+            List<String[]> r = reader.readAll();
+            signals = r.stream().collect(Collectors.toMap(k -> k[0], v -> SignalDto.builder()
+                .type(v[1])
+                .comment(v[2])
+                .exhauster(v[3])
+                .active(v[4])
+                .build()));
+            signals.entrySet().forEach(x -> log.info(String.valueOf(x)));
+        } catch (IOException | CsvException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) {

2949e7667fd7c917fd3434c45a5f357f28156d5d
Rustam Kuramshin <leo.in.empty@gmail.com>
2023-02-19 01:41:34 +0300

    commit

--- refresh.sh
+++ refresh.sh
@@ -2,7 +2,7 @@
 
 git fetch && git checkout main && git pull && \
 
-docker compose rm -f -s exhauster-monitoring-api exhauster-monitoring-ui && \
+docker compose rm -f -s exhauster-monitoring-api exhauster-monitoring-ui grafana && \
 
 docker rmi -f evraz-hackaton-2023-exhauster-monitoring-ui:latest && \
 docker rmi -f exhauster-monitoring:1.0.0 && \
@@ -16,6 +16,6 @@ docker compose up -d mongodb-secondary && \
 docker compose up -d mongodb-arbiter && \
 docker compose up -d influxdb && \
 docker compose up -d postgres_db && \
-docker compose up -d grafana && \
+docker compose build grafana --no-cache && docker compose up -d grafana && \
 docker compose up -d exhauster-monitoring-api && \
 docker compose up -d exhauster-monitoring-ui

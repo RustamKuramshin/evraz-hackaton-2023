78ee71bed1fc1b860bebe8bf744315d8d690239f
Rustam Kuramshin <leo.in.empty@gmail.com>
2023-02-18 23:57:47 +0300

    commit

--- refresh.sh
+++ refresh.sh
@@ -5,6 +5,7 @@ git fetch && git checkout main && git pull
 docker compose rm -f -s exhauster-monitoring-api exhauster-monitoring-ui
 
 docker rmi -f evraz-hackaton-2023-exhauster-monitoring-ui:latest
+docker rmi -f exhauster-monitoring:1.0.0
 
 cd backend
 
